e20f241bbce1fe12ec419007efd8fb0a717056c5
Adrian Castillo <david.marcanao07@gmail.com>
2023-09-13 20:55:58 -0300

    upload

---
+++ modulo2/aula01.py
@@ -0,0 +1,14 @@
+teste = input('Digite su nome >> ')
+
+def ola(a):
+
+    nome = a
+    return nome
+
+print(ola(teste))
+
+
+def chao(a):
+    print(a)
+
+chao('Valor do parametro e passado na chama da funcao')
